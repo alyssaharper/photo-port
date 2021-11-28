55392e089bd7e780fb5971dfa21258b35241247f
Alyssa harper <alyssaharper@Alyssas-Air.home>
2021-11-28 17:23:04 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <div>
       <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
