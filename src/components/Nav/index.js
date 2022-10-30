555ce49f586e8925b2d4be6255d1d5d242b3b586
Alyssa harper <alyssaharper@Alyssas-MacBook-Air.local>
2022-10-30 16:51:26 -0400

    Updated navbar with names and mapping function

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
 function Nav () {
+  const categories = [
+    {
+      name: "commercial",
+      description: "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    {
+      name: "portraits",
+      description: "Portraits of people in my life",
+    },
+    {
+      name: "food",
+      description: "Delicious delicacies",
+    },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
+  function categorySelected(name) {
+    console.log(`${name} clicked`)
+  }
+
   return(
     <header>
       <h2>
@@ -15,9 +38,16 @@ function Nav () {
               About me
             </a>
           </li>
-          <li>
+          <li> 
             <span>Contact</span>
           </li>
+          {categories.map((category) => (
+            <li className='mx-1' key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
